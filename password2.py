67c55e681d9d835f47bef0a6fc216ab88434d350
lauw14 <lauw14@hotmail.com>
2022-12-24 15:42:16 +0800

    ver 2

---
+++ password2.py
@@ -0,0 +1,12 @@
+password = 'a123456'
+i = 3
+while i > 0 :
+	pwd = input('請輸入密碼')
+	if pwd == password:
+		print('登入成功！')
+		break #逃出迴圈
+	else:
+		i = i - 1
+		print('密碼錯誤！還有',i , '次')
+if pwd != password :
+	print('沒輸入機會了，掰掰')
